1f85621c911c9f3cd1eb1c8bbaf74ae72af2882c
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-03-17 17:03:05 +0000

    poker.spec.js test pass refactored

--- src/poker.js
+++ src/poker.js
@@ -11,43 +11,60 @@
 // ['J', 'Q'] -> look at 'J', { 'J': 11 } , look at 'Q', { 'Q': 12 }
 // Do that for both arrays, compare the total score
 // Then return the array with the highest score
+const cardDeckPoints = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14
+}
 
 class Poker {
   // Write your implementation here
-  constructor (card) {
-    this.card = card
-    // this.card2 = card2
-    this.cardDeckPoints = {
-      1: 1,
-      2: 2,
-      3: 3,
-      4: 4,
-      5: 5,
-      6: 6,
-      7: 7,
-      9: 9,
-      10: 10,
-      J: 11,
-      Q: 12,
-      K: 13,
-      A: 14
-    }
+  // constructor () {
+  //   this.cardDeckPoints = {
+  //     1: 1,
+  //     2: 2,
+  //     3: 3,
+  //     4: 4,
+  //     5: 5,
+  //     6: 6,
+  //     7: 7,
+  //     9: 9,
+  //     10: 10,
+  //     J: 11,
+  //     Q: 12,
+  //     K: 13,
+  //     A: 14
+  //   }
+  // }
+
+  isPair (hand) {
+    return hand[0] === hand[1]
   }
 
-  winningPair (card1, card2) {
-    if (((card1[0] === card1[1]) === false) && ((card2[0] === card2[1]) === false)) {
+  winningPair (hand1, hand2) {
+    if (!this.isPair(hand1) && !this.isPair(hand2)) {
       return []
-    } else if (((card1[0] === card1[1]) === true) && ((card2[0] === card2[1]) === false)) {
-      return card1
-    } else if (((card1[0] === card1[1]) === false) && ((card2[0] === card2[1]) === true)) {
-      return card2
+    } else if (this.isPair(hand1) && !this.isPair(hand2)) {
+      return hand1
+    } else if (!this.isPair(hand1) && this.isPair(hand2)) {
+      return hand2
     } else if (
-      this.cardDeckPoints[card1[0]] + this.cardDeckPoints[card1[0]] >
-      this.cardDeckPoints[card2[0]] + this.cardDeckPoints[card2[0]]
+      cardDeckPoints[hand1[0]] + cardDeckPoints[hand1[0]] >
+      cardDeckPoints[hand2[0]] + cardDeckPoints[hand2[0]]
     ) {
-      return card1
+      return hand1
     } else {
-      return card2
+      return hand2
     }
   }
 }
