15b2dccb2264166f75b24197f3ebfe6dcf49d047
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-17 11:53:32 +0100

    made the basic method and passed the test

--- src/poker.js
+++ src/poker.js
@@ -1,5 +1,19 @@
 class Poker {
-  // Write your implementation here
+  winningPair(player1, player2) {
+    if (player1[0] !== player1[1] && player2[0] !== player2[1]) {
+      return [];
+    } else if (player1[0] === player1[1] && player2[0] !== player2[1]) {
+      return player1;
+    } else if (player1[0] !== player1[1] && player2[0] === player2[1]) {
+      return player2;
+    } else {
+      if (player1[0] > player2[0]) {
+        return player1;
+      } else {
+        return player2;
+      }
+    }
+  }
 }
 
-module.exports = Poker
+module.exports = Poker;
