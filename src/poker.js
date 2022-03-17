e26ba015f81ced815db147f2ba6b6558968c294f
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-17 11:35:17 +0000

    Pseudo Code

--- src/poker.js
+++ src/poker.js
@@ -1,5 +1,20 @@
+// Input: 2 arrays of strings, each with 2 items.
+//Output: 1 array.
+
+// Check if both items in an array are the same
+// If they are, it's a pair.
+// If not, it's not a pair.
+// If neither of the arrays has a pair, return []
+// If one of the arrays has a pair, return that pair
+
+// If both of the arrays have pairs, figure out which pair scores higher
+// Check every card in one of the arrays against some kind of point value
+// ['J', 'Q'] -> look at 'J', {'J': 11}, look at 'Q', {'Q': 12}
+// Do that for both arrays, compare the total score
+// Then return the array with the highest score.
+
 class Poker {
   // Write your implementation here
 }
 
-module.exports = Poker
+module.exports = Poker;
