99c441d87ae7c34c441c28459a3abe3a97780f05
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-17 16:21:27 +0000

    First attempt

--- src/poker.js
+++ src/poker.js
@@ -13,8 +13,50 @@
 // Do that for both arrays, compare the total score
 // Then return the array with the highest score.
 
+const cards = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
 class Poker {
-  // Write your implementation here
+  winningPair(handOne, handTwo) {
+    // Check if both items in an array are the same
+    // handOne = ["A", "A"]
+    // handOne [0] === handOne [1]
+    // This is checking if the fst card equal the 2nd card.
+    let handOneIsPair = false; // We don't know if handOne has two cards that are identical.
+    if (handOne[0] === handOne[1]) {
+      // check if first card is equal to second card in handOne
+      // if first card equals 2nd card
+      handOneIsPair = true;
+    }
+
+    let handTwoIsPair = false; // We don't know if handOne has two cards that are identical.
+    if (handTwo[0] === handTwo[1]) {
+      // check if first card is equal to second card in handOne
+      // if first card equals 2nd card
+      handTwoIsPair = true;
+    }
+
+    if (handOneIsPair === false && handTwoIsPair === false) {
+      // If handOne is NOT a pair AND handTwo is NOT a pair, return [].
+      return [];
+    }
+
+    // If they are, it's a pair.
+    // If not, it's not a pair.
+  }
 }
 
 module.exports = Poker;
