0c6c74c9f2e1e506ad8bb9dee9ef9bc15bf8af3a
David <czdavid93@gmail.com>
2022-03-17 17:17:48 +0100

    code criteria test passed

--- src/poker.js
+++ src/poker.js
@@ -1,10 +1,10 @@
 class Poker {
   winningPair (arr1, arr2) {
-    //here i check for no matches.
+    // here i check for no matches.
     if (arr1[0] !== arr1[1] && arr2[0] !== arr2[1]) {
       return []
     }
-    //i used these objects for storing values, and i am going to compare them later.
+    // i used these objects for storing values, and i am going to compare them later.
     const firstSet = {
       values: [],
       match: false,
@@ -30,7 +30,7 @@ class Poker {
       'K',
       'A'
     ]
-//here i am checking if the pair is in arr1 or arr2 and updating the objects above
+    // here i am checking if the pair is in arr1 or arr2 and updating the objects above
     if (arr1[0] === arr1[1]) {
       firstSet.values = arr1
       firstSet.match = true
@@ -42,7 +42,7 @@ class Poker {
       secondSet.match = true
       secondSet.rank = ranks.indexOf(arr2[0])
     }
-//here i am comparing the two objects and return the value of that object
+    // here i am comparing the two objects and return the value of that object
     if (firstSet.match === true && secondSet.match === false) {
       return firstSet.values
     }
@@ -50,7 +50,7 @@ class Poker {
     if (firstSet.match === false && secondSet.match === true) {
       return secondSet.values
     }
-//if both arrays have pairs then the greatest wins.
+    // if both arrays have pairs then the greatest wins.
     if (firstSet.rank > secondSet.rank) {
       return firstSet.values
     }
