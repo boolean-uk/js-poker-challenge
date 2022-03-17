aaa2bbf71334609ca8a7dcfdeea4985bb7e6eee7
MikeCiccia <micheleciccia2@gmail.com>
2022-03-17 12:49:13 +0100

    1comm

--- src/poker.js
+++ src/poker.js
@@ -1,5 +1,19 @@
 class Poker {
-  // Write your implementation here
-}
+ constructor(poker) {
+    this.card1 = card1;
+    this.card2= card2;
+  }
 
-module.exports = Poker
+
+let poker = new Poker()
+poker.winningPair(['Q', '6'], ['J', 'K']) // => []
+
+let poker = new Poker()
+poker.winningPair(['9', '9'], ['7', '7']) // => ['9', '9']
+
+let poker = new Poker()
+poker.winningPair(['K', '3'], ['K', 'K']) // => ['K', 'K']
+  
+
+
+module.exports = Poker;
