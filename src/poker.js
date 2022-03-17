80c03678502bc0846657a1d5c3bcea7bdfa2c62c
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-17 11:37:06 +0000

    basic tests passing

--- src/poker.js
+++ src/poker.js
@@ -1,3 +1,19 @@
-class Poker {}
+class Poker {
+  winningPair(player1, player2) {
+    if (player1[0] !== player1[1] && player2[0] !== player2[1]) {
+      return [];
+    } else if (player1[0] === player1[1] && player2[0] !== player2[1]) {
+      return player1;
+    } else if (player1[0] !== player1[1] && player2[0] === player2[1]) {
+      return player2;
+    } else {
+      if (player1[0] > player2[0]) {
+        return player1;
+      } else {
+        return player2;
+      }
+    }
+  }
+}
 
 module.exports = Poker;
