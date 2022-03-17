fee3aea3220a2bafdfbba1da3dbce16b9eb8ad1f
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-17 12:57:50 +0100

    added comments

--- src/poker.js
+++ src/poker.js
@@ -1,4 +1,5 @@
 class Poker {
+  //convert the letters (A, J, Q, K) to numbers and make all strings to numbers
   convertLettersToNumbers(cards) {
     const cardsInNumbers = [];
     for (let card of cards) {
@@ -28,16 +29,20 @@ class Poker {
     const convertedPlayer1 = this.convertLettersToNumbers(player1);
     const convertedPlayer2 = this.convertLettersToNumbers(player2);
 
+    // If there are no pairs, return an empty array
     return convertedPlayer1[0] !== convertedPlayer1[1] &&
       convertedPlayer2[0] !== convertedPlayer2[1]
       ? []
-      : convertedPlayer1[0] === convertedPlayer1[1] &&
+      : // else if, player 1 has a pair but player 2 not,
+      convertedPlayer1[0] === convertedPlayer1[1] &&
         convertedPlayer2[0] !== convertedPlayer2[1]
       ? player1
-      : convertedPlayer1[0] !== convertedPlayer1[1] &&
+      : // else if, player 2 has a pair but player 1 not
+      convertedPlayer1[0] !== convertedPlayer1[1] &&
         convertedPlayer2[0] === convertedPlayer2[1]
       ? player2
-      : convertedPlayer1[0] > convertedPlayer2[0]
+      : // else, return the player that has a bigger number
+      convertedPlayer1[0] > convertedPlayer2[0]
       ? player1
       : player2;
   }
