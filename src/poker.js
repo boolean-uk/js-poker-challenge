a367133e6125c9f1593b7e646f746d13f6f8cdd1
Chris Hyde <c_hyde333@hotmail.com>
2022-03-17 11:15:34 +0000

    Object created for inputs & outputs

--- src/poker.js
+++ src/poker.js
@@ -1,3 +1,33 @@
+// Input: 2 arrays of strings, each with 2 iteams
+// Output: One array
+
+// Check if both iteams in array are the same
+// If they are, it's a pair
+// If not, it's not a pair
+// If neither of the arrays have pairs, return []
+// If one of the arrays has a pair, return that pair
+
+// If both of the arrays has pairs, figure out which pair scores higher
+// Check every card in an array against some kind of point value
+// Do that for both arrays, compare the total score
+// then return the array with the highest score
+
+// const pointMap = {
+//   Card2: 2,
+//   Card3: 3,
+//   Card4: 4,
+//   Card5: 5,
+//   Card6: 6,
+//   Card7: 7,
+//   Card8: 8,
+//   Card9: 9,
+//   Card10: 10,
+//   CardJack: 11,
+//   CardQueen: 12,
+//   CardKing: 13,
+//   CardAce: 14
+// }
+
 class Poker {
   // Write your implementation here
 }
