af677d14c2866ab1ec142b163de23970cbec2ffc
SeanRichhh <sean-richards@outlook.com>
2022-03-17 15:36:38 +0000

    setup

--- src/poker.js
+++ src/poker.js
@@ -1,5 +1,35 @@
-class Poker {
-  // Write your implementation here
+// input : 2 arrays of strings, each with 2 items
+// Output: One array
+
+// Check if both items in an array are the same/
+// if they are its a pair
+// if not its not  a pair
+// if neither of the arrays have pairs , return []
+// if one of the arrays had a pair , return that pair
+
+// if both of the arrays have pairs, figutre out which pair scores higher
+// Check every card in an array against some kind of point value
+// Do that for both arrays compare the total score then return the array with the highest score
+const cardScores = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14
 }
 
+class Poker {
+  winningpair (handOne, handTwo) {
+  }
+}
+
+// Write your implementation here
 module.exports = Poker
