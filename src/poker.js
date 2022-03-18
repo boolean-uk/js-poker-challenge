f63bdf9778cc892cbd8fb3c887e4b715a41a5937
AlexDjangoX <alexmonk17@gmail.com>
2022-03-18 11:24:16 +0100

    removed else statements

--- src/poker.js
+++ src/poker.js
@@ -29,18 +29,20 @@ class Poker {
   winningPair(array1, array2) {
     if (array1[0] !== array1[1] && array2[0] !== array2[1]) {
       return [];
-    } else if (array1[0] === array1[1] && array2[0] !== array2[1]) {
+    }
+    if (array1[0] === array1[1] && array2[0] !== array2[1]) {
       return array1;
-    } else if (array1[0] !== array1[1] && array2[0] === array2[1]) {
+    }
+    if (array1[0] !== array1[1] && array2[0] === array2[1]) {
       return array2;
-    } else if (
+    }
+    if (
       this.scoreMap[array1[0]] + this.scoreMap[array1[0]] >
       this.scoreMap[array2[0]] + this.scoreMap[array2[0]]
     ) {
       return array1;
-    } else {
-      return array2;
     }
+    return array2;
   }
 
   winningPairFromArray(array) {
@@ -48,15 +50,15 @@ class Poker {
     for (let element of array) {
       if (element[0] !== element[1]) {
         returnArray.push(0);
-      } else if (element[0] === element[1]) {
+      }
+      if (element[0] === element[1]) {
         returnArray.push(this.scoreMap[element[0]] + this.scoreMap[element[1]]);
       }
     }
     if (returnArray.reduce((x, y) => x + y, 0) === 0) {
       return [];
-    } else {
-      return array[returnArray.indexOf(Math.max(...returnArray))];
     }
+    return array[returnArray.indexOf(Math.max(...returnArray))];
   }
   //   = [
   //   ["5", "5", "3"],
@@ -85,36 +87,10 @@ class Poker {
 module.exports = Poker;
 
 const hand = new Poker();
-console.log(
-  hand.winning3CardHand([
-    ["5", "5", "3"],
-    ["A", "A"],
-    ["7", "7", "7"],
-    ["Q", "J", "9"]
-  ])
-);
 
-// console.log("returnArray.....", returnArray);
-//     console.log(
-//       "index of winner......",
-//       returnArray.indexOf(Math.max(...returnArray))
-//     );
-
-// if (Array.isArray(array) && array.length === 1) {
-//   if (array[0][0] !== array[0][1] && array[1][0] !== array[1][1]) {
-//     return [];
-//   }
-// } else {
-//   return [];
-// }
-// }
-
-// winningPairFromArray(array) {
-//   if (array[0][0] !== array[0][1] && array[1][0] !== array[1][1]) {
-//     return [];
-//   } else if (array[0][0] === array[0][1] && array[1][0] !== array[1][1]) {
-//     return array[1];
-//   } else if (array[0][0] !== array[0][1] && array[1][0] === array[1][1]) {
-//     return array[0];
-//   }
-// }
+hand.winning3CardHand([
+  ["5", "5", "3"],
+  ["A", "A"],
+  ["7", "7", "7"],
+  ["Q", "J", "9"]
+]);
