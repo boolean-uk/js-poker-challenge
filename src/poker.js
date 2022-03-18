85862bea7e361a11eb01651106a35254e235bd5b
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-18 16:45:55 +0000

    Third attempt passed

--- src/poker.js
+++ src/poker.js
@@ -1,19 +1,3 @@
-// Input: 2 arrays of strings, each with 2 items.
-//Output: 1 array.
-
-// Check if both items in an array are the same
-// If they are, it's a pair.
-// If not, it's not a pair.
-// If neither of the arrays has a pair, return []
-// If one of the arrays has a pair, and the other one doesn't, return the pair.
-// e.g. If handOne id a pair, and handTwo is not a pair, return handOne. And vice versa
-
-// If both of the arrays have pairs, figure out which pair scores higher
-// Check every card in one of the arrays against some kind of point value
-// ['J', 'Q'] -> look at 'J', {'J': 11}, look at 'Q', {'Q': 12}
-// Do that for both arrays, compare the total score
-// Then return the array with the highest score.
-
 const cards = {
   2: 2,
   3: 3,
@@ -47,13 +31,18 @@ class Poker {
     }
 
     if (handOneIsPair === false && handTwoIsPair === true) {
-      return handTwoIsPair;
+      return handTwo;
     }
 
     if (handOneIsPair === true && handTwoIsPair === false) {
-      return handOneIsPair;
+      return handOne;
+    }
+    if (
+      cards[handOne[0]] + cards[handOne[1]] >
+      cards[handTwo[0]] + cards[handTwo[1]]
+    ) {
+      return handOne;
     }
   }
 }
-
 module.exports = Poker;
