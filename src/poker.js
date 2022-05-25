48bee18be3451a379745d816d959f3beebace868
John Xander <johnblox5@gmail.com>
2022-05-25 18:50:36 +0100

    Winning Pair From Array ✅

--- src/poker.js
+++ src/poker.js
@@ -1,14 +1,50 @@
 class Poker {
 
   constructor() {
-    this.name = "Poker"
+    this.isPair = p => p[0] === p[1]
   }
 
   winningPair(pair1, pair2) {
-    const isPair = p => p[0] === p[1]
-    const higherCard = (p1, p2) => p1[0] > p2[0] ? p1 : p2
-    return isPair(pair1) && isPair(pair2) ? higherCard(pair1, pair2) :
-      isPair(pair1) ? pair1 : pair2
+
+    let result 
+
+    if (this.isPair(pair1)) result = pair1
+    if (this.isPair(pair2)) result = pair2 
+
+    return !this.isPair(pair1) && !this.isPair(pair2) ? [] : result
+
+  }
+
+  winningPairFromArray(arr) {
+
+    const realPairs = arr.filter(pair => this.isPair(pair))
+
+    const sortedNonRoyals = realPairs.filter(ps => {
+      return !ps.includes("J") && !ps.includes("Q") &&
+      !ps.includes("K") && !ps.includes("A")
+    }).sort()
+
+    const sortedRoyals = []
+    const royals = realPairs.filter(ps => {
+      return ps.includes("J") || ps.includes("Q") ||
+      ps.includes("K") || ps.includes("A")
+    })
+    royals.map(ps => {
+      if(ps.includes("J")) sortedRoyals.push(ps)
+    })
+    royals.map(ps => {
+      if(ps.includes("Q")) sortedRoyals.push(ps)
+    })
+    royals.map(ps => {
+      if(ps.includes("K")) sortedRoyals.push(ps)
+    })
+    royals.map(ps => {
+      if(ps.includes("A")) sortedRoyals.push(ps)
+    })
+
+    const sortedRealPairs = sortedNonRoyals.concat(sortedRoyals)
+    return realPairs.length > 0 ? sortedRealPairs[sortedRealPairs.length - 1] : realPairs
+    
   }
 }
 
