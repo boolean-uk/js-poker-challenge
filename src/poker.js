76f842aa91f86920c3a3c43e0c9dca8310bb9d80
John Xander <johnblox5@gmail.com>
2022-05-25 17:04:11 +0100

    Winning Pair

--- src/poker.js
+++ src/poker.js
@@ -1,7 +1,15 @@
 class Poker {
-  // Write your implementation here
 
-  // Initial commit and pull request
+  constructor() {
+    this.name = "Poker"
+  }
+
+  winningPair(pair1, pair2) {
+    const isPair = p => p[0] === p[1]
+    const higherCard = (p1, p2) => p1[0] > p2[0] ? p1 : p2
+    return isPair(pair1) && isPair(pair2) ? higherCard(pair1, pair2) :
+      isPair(pair1) ? pair1 : pair2
+  }
 }
 
 module.exports = Poker
