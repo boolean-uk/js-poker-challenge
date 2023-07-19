bfc60566e4140c8da54e21e1a8bc51e35a6980e1
karim <01171252@pw.edu.pl>
2023-07-19 14:53:58 +0200

    extn 1

--- src/poker.js
+++ src/poker.js
@@ -1,8 +1,34 @@
-function winningPair() {}
+function isPair(cards) {
+  return cards.length === 2 && cards[0] === cards[1]
+}
+
+function winningPair(pair1, pair2) {
+  if (!isPair(pair1) && !isPair(pair2)) {
+    return []
+  }
+
+  const getValue = (card) => {
+    const values = { A: 14, K: 13, Q: 12, J: 11 }
+    return isNaN(card) ? values[card] : parseInt(card, 10)
+  }
+
+  const pair1Value = isPair(pair1) ? getValue(pair1[0]) : 0
+  const pair2Value = isPair(pair2) ? getValue(pair2[0]) : 0
+
+  return pair1Value >= pair2Value ? pair1 : pair2
+}
 
 // Extension criteria
+function winningPairFromArray(pairsArray) {
+  if (!Array.isArray(pairsArray) || pairsArray.length === 0) {
+    return []
+  }
 
-function winningPairFromArray() {}
+  return pairsArray.reduce((currentWinningPair, pair) => {
+    const currentPair = winningPair(currentWinningPair, pair)
+    return currentPair
+  }, [])
+}
 
 function winning3CardHand() {}
 
