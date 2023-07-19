1c942837aa500c62e52e7a2e448d370142d34c95
Pawel Kwiecinski <pawel.kwiecinski@talan.com>
2023-07-19 13:59:12 +0200

    Extension2

--- src/poker.js
+++ src/poker.js
@@ -45,7 +45,41 @@ function winningPairFromArray(pairArray) {
   return [highestString, highestString]
 }
 
-function winning3CardHand() {}
+function getHighest(resultArray) {
+  let highestString = '3'
+  for (const i in resultArray) {
+    if (cardMap[resultArray[i]] > cardMap[highestString])
+      highestString = resultArray[i]
+  }
+  return highestString
+}
+
+function winning3CardHand(pairArray) {
+  const resultArray = []
+  const result3Array = []
+
+  for (const i in pairArray) {
+    if (
+      pairArray[i].length === 3 &&
+      pairArray[i][0] === pairArray[i][1] &&
+      pairArray[i][0] === pairArray[i][2]
+    ) {
+      result3Array.push(pairArray[i][0])
+    } else if (pairArray[i][0] === pairArray[i][1])
+      resultArray.push(pairArray[i][0])
+  }
+
+  if (result3Array.length !== 0) {
+    const highestString = getHighest(result3Array)
+    return [highestString, highestString, highestString]
+  }
+
+  if (resultArray.length === 0) return []
+
+  const highestString = getHighest(resultArray)
+
+  return [highestString, highestString]
+}
 
 module.exports = {
   winningPair,
