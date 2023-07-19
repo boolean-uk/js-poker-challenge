67d3fbbf84992c90140d23b8600a3fe4e960e735
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-19 14:11:10 +0200

    Refactor

--- src/poker.js
+++ src/poker.js
@@ -14,22 +14,31 @@ const cardValue = (card) => {
   return pointsMap[card] || parseInt(card)
 }
 
-const isPair = (pair) => {
-  const [c1, c2] = pair
-  if (pair.length !== 2 || !isCard(c1) || !isCard(c2)) {
+const validatePair = (pair) => {
+  if (pair.length !== 2 || isCard(pair[0]) || isCard(pair[1])) {
     throw new Error(`Invalid pair: ${pair}`)
   }
+}
+
+const isPair = (pair) => {
+  validatePair(pair)
+  const [c1, c2] = pair
   return c1 === c2
 }
 
+const pairValue = (pair) => {
+  validatePair(pair)
+  const [c1, c2] = pair
+  return cardValue(c1) + cardValue(c2)
+}
 
 function winningPair(pair1, pair2) {
   if (isPair(pair1) !== isPair(pair2)) {
     return isPair(pair1) ? pair1 : pair2
   }
   if (isPair(pair1) && isPair(pair2)) {
-    const pair1Value = cardValue(pair1[0]) + cardValue(pair1[1])
-    const pair2Value = cardValue(pair2[0]) + cardValue(pair2[1])
+    const pair1Value = pairValue(pair1)
+    const pair2Value = pairValue(pair2)
     return pair1Value > pair2Value ? pair1 : pair2
   }
   return []
