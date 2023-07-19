02abd0f7f79523d84b42dab276d2520ce1b9b0f6
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-19 14:10:26 +0200

    winnirpar method done

--- src/poker.js
+++ src/poker.js
@@ -1,4 +1,21 @@
-function winningPair() {}
+const cardPair  = {
+  J: '11',
+  Q: '12',
+  K: '13',
+  A: '14'
+}
+
+const OneIsPair = (cards) => cards[0] === cards[1]
+const isHigher = (cards1, cards2) => cards1[0] > cards2[0] ? cards1 : cards2
+
+function winningPair(pair1, pair2) {
+  let result 
+  if (OneIsPair(pair1)) result = pair1
+  if (OneIsPair(pair2)) result = pair2 
+  return !OneIsPair(pair1) && !OneIsPair(pair2) ? [] : 
+    OneIsPair(pair1) && OneIsPair(pair2) ? isHigher(pair1, pair2) :
+      result
+}
 
 // Extension criteria
 
