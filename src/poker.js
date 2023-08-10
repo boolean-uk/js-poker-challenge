373dc7177fb3096afd9011cbaf0f66e8d064c82d
ks46 <konastaf46@gmail.com>
2023-08-10 22:39:38 +0300

    Code for winningPair() function

--- src/poker.js
+++ src/poker.js
@@ -1,4 +1,18 @@
-function winningPair() {}
+function winningPair(pair1, pair2) {
+  const isPair1 = pair1[0] === pair1[1]
+  const isPair2 = pair2[0] === pair2[1]
+
+  if (isPair1 && isPair2) {
+    return pair1[0] > pair2[0] ? pair1 : pair2
+  }
+  if (isPair1) {
+    return pair1
+  }
+  if (isPair2) {
+    return pair2
+  }
+  return []
+}
 
 // Extension criteria
 
