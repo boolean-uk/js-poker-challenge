490e05880424140aecad14a35d4c5882bcae2466
ks46 <konastaf46@gmail.com>
2023-08-10 22:59:29 +0300

    Code for winningPairFromArray() function

--- src/poker.js
+++ src/poker.js
@@ -1,9 +1,29 @@
+function isPair(pair) {
+  return pair[0] === pair[1]
+}
+
+function symbolNumber(symbol) {
+  // find numeric representation of symbol
+  switch (symbol) {
+    case 'J':
+      return 11
+    case 'Q':
+      return 12
+    case 'K':
+      return 13
+    case 'A':
+      return 14
+    default:
+      return parseInt(symbol)
+  }
+}
+
 function winningPair(pair1, pair2) {
-  const isPair1 = pair1[0] === pair1[1]
-  const isPair2 = pair2[0] === pair2[1]
+  const isPair1 = isPair(pair1)
+  const isPair2 = isPair(pair2)
 
   if (isPair1 && isPair2) {
-    return pair1[0] > pair2[0] ? pair1 : pair2
+    return symbolNumber(pair1[0]) > symbolNumber(pair2[0]) ? pair1 : pair2
   }
   if (isPair1) {
     return pair1
@@ -16,7 +36,28 @@ function winningPair(pair1, pair2) {
 
 // Extension criteria
 
-function winningPairFromArray() {}
+function winningPairFromArray(pairs) {
+  // parse array of pairs, keeping track of current winning pair
+  let winningIndex = -1
+  let maxNum = 0
+
+  for (let i = 0; i < pairs.length; ++i) {
+    const pair = pairs[i]
+    if (isPair(pair)) {
+      const num = symbolNumber(pair[0])
+      if (num > maxNum) {
+        maxNum = num
+        winningIndex = i
+      }
+    }
+  }
+
+  if (winningIndex === -1) {
+    return []
+  }
+
+  return pairs[winningIndex]
+}
 
 function winning3CardHand() {}
 
