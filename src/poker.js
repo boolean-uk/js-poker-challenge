1f3ee6d29384bb38536040eff3492a1afaf3e580
Irene <irene.maniou@hotmail.com>
2023-08-13 21:20:26 +0300

    finished poker exercise

--- src/poker.js
+++ src/poker.js
@@ -1,14 +1,21 @@
 /* eslint-disable prettier/prettier */
 function winningPair(pair1, pair2) {
   if (pair1[0] === pair1[1] && pair2[0] !== pair2[1]) {
-    return pair1;
+    return pair1
   } else if (pair2[0] === pair2[1] && pair1[0] !== pair1[1]) {
-    return pair2;
+    return pair2
   } else if (pair2[0] === pair2[1] && pair1[0] === pair1[1]) {
-    if (pair1[0] > pair2[0]) {
+    if (pair1[0] === 'A') {
       return pair1
-    } else {
+    }
+    else if (pair2[0] === 'A') {
       return pair2
+    } else {
+      if (pair1[0] > pair2[0]) {
+        return pair1
+      } else {
+        return pair2
+      }
     }
   } else {
     return [];
@@ -18,7 +25,7 @@ function winningPair(pair1, pair2) {
 // Extension criteria
 
 function winningPairFromArray(array) {
-  const result = [];
+  let result = [];
   for (let i = 0; i < array.length; i++) {
     if (i + 1 < array.length) {
       const pair = winningPair(array[i], array[i + 1]);
@@ -28,24 +35,37 @@ function winningPairFromArray(array) {
       }
     }
   }
-  return result;
+  if (result.length > 1) {
+    for (let j = 0; j < result.length; j++) {
+      if (j + 1 < result.length) {
+        result = winningPair(result[j], result[j + 1])
+      }
+    }
+  }
+  return result.length === 1 ? result[0] : result;
 }
-console.log(winningPairFromArray([['K', 'K'], ['A', 'A']])); // Output: [['A', 'A']]
-console.log(winningPairFromArray([['4', '3'], ['6', '6'], ['7', '7'], ['3', '3']])); // Output: [['7', '7']]
-console.log(winningPairFromArray([['4', '3'], ['6', '2'], ['7', '1'], ['3', '9']])); // Output: []
 
 function winning3CardHand(array) {
+  let result = []
   for (let i = 0; i < array.length; i++) {
-    if (array[i].length === 2) {
-      return winningPairFromArray(array)
+    if (array[i].length === 2 && !result.includes(winningPairFromArray(array))) {
+      result.push(winningPairFromArray(array))
     } else {
       const newarray = array[i]
       const value = newarray[0]
       if (value === newarray[1] && value === newarray[2]) {
-        return newarray
+        result.push(newarray)
       }
     }
   }
+  if (result.length > 1) {
+    for (let j = 0; j < result.length; j++) {
+      if (result[j].length === 3) {
+        result = result[j]
+      }
+    }
+  }
+  return result.length === 1 ? result[0] : result;
 }
 
 module.exports = {
