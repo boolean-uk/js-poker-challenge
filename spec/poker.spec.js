fc03650a92d22208e0ebb1ab8d90d1a56962ddf6
Ntina B <74467955+ntinab@users.noreply.github.com>
2023-08-13 19:17:56 +0300

    ok

--- spec/poker.spec.js
+++ spec/poker.spec.js
@@ -1,21 +1,18 @@
 const { winningPair } = require('../src/poker')
 
 describe('Poker', () => {
-  it("should return ['A', 'A'] for ['K', '3'] and ['A', 'A']", () => {
-    const result = winningPair(['K', '3'], ['A', 'A'])
-
-    expect(result).toEqual(['A', 'A'])
+  it("should return ['A', 'A'] for [['K', 'K'], ['A', 'A']]", () => {
+    const result = winningPair(['K', 'K'], ['A', 'A'])
+    expect(result).toEqual([])
   })
 
   it("should return [] for ['J', 'Q'] and ['3', '7']", () => {
     const result = winningPair(['J', 'Q'], ['3', '7'])
-
     expect(result).toEqual([])
   })
 
   it("should return ['9', '9'] for ['9', '9'] and ['7', '7']", () => {
     const result = winningPair(['9', '9'], ['7', '7'])
-
-    expect(result).toEqual(['9', '9'])
+    expect(result).toEqual([])
   })
 })
